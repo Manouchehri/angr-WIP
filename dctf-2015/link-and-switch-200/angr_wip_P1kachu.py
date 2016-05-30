2fadf5e80b932ece136657e56827db05ac5741b0
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2016-05-30 19:37:05 +0200

    Add details on how to get infos
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- dctf-2015/link-and-switch-200/angr_wip_P1kachu.py
+++ dctf-2015/link-and-switch-200/angr_wip_P1kachu.py
@@ -1,12 +1,7 @@
-
-# coding: utf-8
-
-# In[ ]:
+#!/usr/bin/python2
 
 import angr
 
-
-# In[ ]:
 
 fail = (0x400947, 0x400958)
 win  = (0x400936,)
@@ -27,21 +22,25 @@ def patch_fgets(state):
         state.mem[addr + x:] = state.se.BVS('c', 8)
 
 
-# In[ ]:
-
 p = angr.Project('r200.bin')
 p.hook(0x40091c, func=patch_fgets, length=5)
 
 
-# In[ ]:
 
 init = p.factory.blank_state()
+
+
+# b * XXXXXXX
+# info proc mapping
+# dump binary memory stack addr
+# dump binary memory heap addr
+# info registers
 
 init.gdb.set_stack('binaries/r200/stack', stack_top=0x7ffffffde000)
 print("Stack set")
 
 init.gdb.set_heap('binaries/r200/heap', heap_base=0x602000)
-print("Heap set") 
+print("Heap set")
 
 # https://github.com/angr/simuvex/blob/efa097d4076401cbd48277223e1340d7c6dffbc1/simuvex/plugins/gdb.py#L97
 # Some registers such as cs, ds, eflags etc. aren't supported in Angr
@@ -49,12 +48,7 @@ init.gdb.set_regs('binaries/r200/regs')
 print("Registers set")
 
 
-# In[ ]:
-
 pgp = p.factory.path_group(init, threads=8)
-
-
-# In[ ]:
 
 ex = pgp.explore(find=win, avoid=fail)
 
